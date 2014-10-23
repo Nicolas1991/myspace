7517fc0ff4e3ca097aea74dfcb9b9b1b423cccbc
ZHANGYAN <zhangyan@dyn038230.shef.ac.uk>
2014-10-23 14:40:02 +0100

    Initial Commit

---
+++ 123/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  123
+//
+//  Created by  ZHANGYAN on 14-10-23.
+//  Copyright (c) 2014年  ZHANGYAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 123/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  123
+//
+
+//  Copyright (c) 2014年  ZHANGYAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
